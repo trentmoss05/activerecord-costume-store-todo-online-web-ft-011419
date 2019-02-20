11add5921581efd742cb8adca23966252432b204
Trent Moss <trentmoss05@gmail.com>
2019-02-20 02:26:28 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -15,7 +15,7 @@ class CreateCostumes < ActiveRecord[4.2]
       t.float :price
       t.string :size
       t.string :image_url
-      t.timestamps null: false
+      t.timestamps
     end
   end
 end
