d9a8db7d02e4f6cb8d360e03e3a019bd8ad9318b
Trent Moss <trentmoss05@gmail.com>
2019-02-20 02:30:10 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -26,9 +26,9 @@ ActiveRecord::Schema.define(version: 3) do
 
   create_table "costumes", force: :cascade do |t|
     t.string "name"
+    t.float "price"
     t.string "size"
     t.string "image_url"
-    t.float "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
